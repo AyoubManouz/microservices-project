010ed9c42b5672d67603f84636fba8adb87dd9fc
Ayoub Manouz <ayoubmanouz2015@gmail.com>
2023-11-27 09:56:16 -0500

    Implement event driven architecture using Kafka

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -3,12 +3,14 @@ package com.example.orderservice.service;
 import com.example.orderservice.dto.InventoryResponse;
 import com.example.orderservice.dto.OrderLineItemsDto;
 import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.event.OrderPlacedEvent;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.model.OrderLineItems;
 import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -23,6 +25,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -48,12 +51,11 @@ public class OrderService {
             if(!isAllInStock)
                 throw new IllegalArgumentException("Some Products are not in stock, please try again later");
             orderRepository.save(order);
+            kafkaTemplate.sendDefault(new OrderPlacedEvent(order.getOrderNumber()));
             return "Order Placed Successfully";
         } finally {
             inventoryServiceLookup.end();
         }
-
-
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
